8ac02aa86ea8f32a4650c136bdec42553fb2a170
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2023-07-02 14:16:58 -0400

    Configure network (#3)

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "main" {}
+
+resource "aws_route_table" "main" {}
+
+resource "internet_gateway" "main" {}
